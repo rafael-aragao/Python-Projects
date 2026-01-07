37eb38b1e7ef15046608f84e1e3b095bc79994c2
rafael-aragao <67762764+rafael-aragao@users.noreply.github.com>
2026-01-07 17:19:16 -0300

    Add files via upload

---
+++ 01-Básico/Laços/exe003.py
@@ -0,0 +1,2 @@
+for i in range(1, 11):
+    print(f"3 X {i} = {3 * i}")
---
+++ 01-Básico/Laços/exe004.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um numero (0 para sair): "))
+while numero != 0:
+    print(f"Você digitou {numero}")
+    numero = int(input("Digite um numero (0 para sair): "))
